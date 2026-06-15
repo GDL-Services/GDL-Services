3c09a68247d7eb801d7fec315448a1a582deb49c
griguoligiuseppe-gif <griguoli.giuseppe@gmail.com>
2026-06-15 21:34:53 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -9,10 +9,10 @@
    dispositivo (modalità Locale).
    ============================================================ */
 window.FIREBASE_CONFIG = {
-  // apiKey: "............",
+  // apiKey: "AIzaSyCF8Y9vu-BizStlFBgI2__bJvHx-HEJJ_c",
   // authDomain: "gdl-services.firebaseapp.com",
   // projectId: "gdl-services",
-  // storageBucket: "gdl-services.appspot.com",
-  // messagingSenderId: "............",
-  // appId: "............"
+  // storageBucket: "gdl-services.firebasestorage.app",
+  // messagingSenderId: "880746254995",
+  // appId: "1:880746254995:web:4fb4b7dda62af94d28f366"
 };
